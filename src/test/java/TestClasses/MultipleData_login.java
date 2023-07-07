d1f424ddcc3d3cce960bbc491280c4aed02f0eca
Baliram Korsule <138753198+korsulebk@users.noreply.github.com>
2023-07-07 16:40:07 +0530

    Update MultipleData_login.java

--- src/test/java/TestClasses/MultipleData_login.java
+++ src/test/java/TestClasses/MultipleData_login.java
@@ -5,6 +5,7 @@ public class MultipleData_login {
 	public static void main(String[]args) {
 		System.out.println("new test case");
 		System.out.println("see what will happen");
+		System.out.println("heyyy");
 		
 	}
 
